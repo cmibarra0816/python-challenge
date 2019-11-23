ee5e1346453d496601ce8ea4a20674a131819d1e
Claudia Ibarra <Claudi@Claudias-MacBook-Pro.local>
2019-11-23 16:54:46 -0600

    add

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,10 @@
 #claudia ibarra
 #python-challenge hw
 
+#python-challenge is to create a python script that analyzes the records to calculate:
+#number of months including the dataset, net total amount of "profit/losses"
+#average of changes/greatest increase/greatest decrease in the "profit/losses"
+
 #import modules
 import os
 import csv
@@ -8,12 +12,32 @@ import csv
 budget_csv = os.path.join("../PyBank","budget_data.csv")
 
 output_file = os.path.join("Bank Report")
+#in the bank report, looking for:
+#total months
+#total amount
+#average change
+#greatest increase in profits month / amount
+#greatest decrease in profits month / amount
+
 #tee-ing up with variables
 total_months = 0
+total_profit = 0
+profit = []
+previous_profit = 0
+month_of_change = []
+profit_change = 0
+greatest_decrease = ["", 9999999]
+greatest_increase = ["", 0]
+profit_change_list = []
+profit_average = 0
 
 #open budget csv file 
 with open(budget_csv, 'r') as Bank_Report:
     csvreader = csv.DictReader(Bank_Report)
     for row in csvreader:
        total_months = total_months + 1
+       
+       total_profit = total_profit + int(row["Profit/Losses"])
+
 print (total_months)
+print (total_profit)
