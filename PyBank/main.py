9ac290888101050951ed929faac88bc6d6236c36
Claudia Ibarra <Claudi@Claudias-MacBook-Pro.local>
2019-11-26 20:04:46 -0600

    add

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,8 +2,8 @@
 #python-challenge hw
 
 #python-challenge is to create a python script that analyzes the records to calculate:
-#number of months including the dataset, net total amount of "profit/losses"
-#average of changes/greatest increase/greatest decrease in the "profit/losses"
+#number of months including the dataset; net total amount of "profit/losses" overall;
+#average of changes the "profit/losses" overall; greatest increase / decrease overall
 
 #import modules
 import os
@@ -36,15 +36,32 @@ with open(budget_csv, 'r') as Bank_Report:
     csvreader = csv.DictReader(Bank_Report)
     for row in csvreader:
        #calculating total months
-       total_months = total_months + 1
-       #calculating total profit
-       total_profit = total_profit + int(row["Profit/Losses"])
+        total_months = total_months + 1
+       #calculating total profit 
+        total_profit = total_profit + int(row["Profit/Losses"])
        #calculating profit change
-       profit_change = float(row["Profit/Losses"]) - previous_profit
+        profit_change = float(row["Profit/Losses"]) - previous_profit
        #calculating previous profit
-       previous_profit = float(row["Profit/Losses"])
+        previous_profit = float(row["Profit/Losses"])
+       #calculating profit change list
+        profit_change_list = profit_change_list + [profit_change]
+       #calculating change of months
+        month_of_change = [month_of_change] + [row["Date"]]
+        if profit_change > greatest_increase[1]:
+            greatest_increase[1]= profit_change
+            greatest_increase[0] = row['Date']
+        if profit_change < greatest_decrease[1]:
+            greatest_decrease[1]= profit_change
+            greatest_decrease[0] = row['Date']
+    #calculating profit average
+    profit_average = round(sum(profit_change_list[1:])/len(profit_change_list[1:]), 2)
 
-print (total_months)
-print (total_profit)
-print (profit_change)
-print (previous_profit)
+#write changes to csv
+with open(budget_csv, 'w') as Bank_Report:
+    Bank_Report.write("Financial Analysis\n")
+    Bank_Report.write("---------------------\n")
+    Bank_Report.write("Total Months: %d\n" % total_months)
+    Bank_Report.write("Total Profit: $%d\n" % total_profit)
+    Bank_Report.write("Average Profit Change $%d\n" % profit_average)
+    Bank_Report.write("Greatest Increase in Profit: %s ($%s)\n" % (greatest_increase[0], greatest_increase[1]))
+    Bank_Report.write("Greatest Decrease in Profit: %s ($%s)\n" % (greatest_decrease[0], greatest_decrease[1]))
